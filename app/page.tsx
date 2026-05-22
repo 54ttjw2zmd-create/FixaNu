9b14ac439d0a94d643a38819249b6fb1898af56a
Robin South <robinsouth@Robin-South-MacBook-Pro-2.local>
2026-05-22 14:00:16 +0200

    add thank you page

--- app/page.tsx
+++ app/page.tsx
@@ -28,16 +28,22 @@ export default function Page() {
           </p>
 
           <form
-             action="https://formspree.io/f/mnjrvnwa"
-             method="POST"
-             className="flex flex-col sm:flex-row gap-4">
+            action="https://formspree.io/f/mnjrvnwa"
+            method="POST"
+            className="flex flex-col sm:flex-row gap-4"
+>
            <input
-              type="hidden"
-              name="_next"
-              value="https://fixanu.nu/thank-you"
-              placeholder="Din e-postadress"
-              className="cabinet-medium flex-1 rounded-2xl bg-[#F8FAFC] border border-slate-200 px-5 py-4 text-[#0B2341] placeholder:text-slate-400 outline-none focus:ring-2 focus:ring-emerald-400"
-            />
+            type="hidden"
+            name="_next"
+            value="https://fixanu.nu/thank-you"
+  />
+
+  <input
+    type="email"
+    name="email"
+    placeholder="Din e-postadress"
+    className="cabinet-medium flex-1 rounded-2xl bg-[#F8FAFC] border border-slate-200 px-5 py-4 text-[#0B2341] placeholder:text-slate-400 outline-none focus:ring-2 focus:ring-emerald-400"
+  />
 
             <button
               type="submit"
